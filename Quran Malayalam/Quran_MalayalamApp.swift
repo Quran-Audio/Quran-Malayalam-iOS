31a931a819449c0eece03bf7dc61306ae205d49e
Mohammed Shafeer <shafeer@digivalsolutions>
2022-01-09 08:06:34 +0530

    Initial Commit

---
+++ Quran Malayalam/Quran_MalayalamApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quran_MalayalamApp.swift
+//  Quran Malayalam
+//
+//  Created by Mohammed Shafeer on 09/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Quran_MalayalamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
