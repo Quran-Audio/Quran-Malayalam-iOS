bc2770901c89f24579d3663c6a914de17371d4ee
Mohammed Shafeer <shafeer@digivalsolutions>
2022-02-12 13:30:04 +0530

    All praise is to ALLAH - request rating service fixed

--- Quran Malayalam/Quran_MalayalamApp.swift
+++ Quran Malayalam/Quran_MalayalamApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import QuranAudioPlayerKit
+import StoreKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
@@ -21,7 +22,6 @@ struct Quran_MalayalamApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     init() {
-        RatingService.shared.checkAndAskForReview()
         PushService.shared.askForPushPermission()
         QuranAudioPlayerKit.registerFonts()
     }
@@ -29,7 +29,16 @@ struct Quran_MalayalamApp: App {
     var body: some Scene {
         WindowGroup {
             QuranAudioPlayerKit()
+                .onAppear(perform: { requestReviewIfNeeded() })
                 .preferredColorScheme(.light)
+        }
+    }
+    
+    private func requestReviewIfNeeded() {
+        let shoudlRequest = RatingService.shared.shouldRequest()
+        if shoudlRequest == true,
+           let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+            SKStoreReviewController.requestReview(in: scene)
         }
     }
 }
